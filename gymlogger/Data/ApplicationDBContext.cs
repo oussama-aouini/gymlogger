7ae745d22198de686f1f632d741dc2abec199e9a
Ousaama Aouini <oussama.aouini.dev@gmail.com>
2024-10-26 16:51:58 +0100

    added dbcontext

---
+++ gymlogger/Data/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using gymlogger.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace gymlogger.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions dbContextOptions)
+            : base(dbContextOptions)
+        {
+            
+        }
+
+        DbSet<Exercise> Exercises { get; set; }
+        DbSet<Set> Sets { get; set; }
+        DbSet<WorkoutSession> WorkoutSessions { get; set; }
+        DbSet<WorkoutTemplate> workoutsTemplates { get; set; }
+    }
+}
---
+++ gymlogger/Migrations/20241026154754_InitialCreate.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace gymlogger.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Exercises",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Exercises", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WorkoutSessions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StartTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkoutSessions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "workoutsTemplates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_workoutsTemplates", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Weight = table.Column<float>(type: "real", nullable: false),
+                    Repetitions = table.Column<int>(type: "int", nullable: false),
+                    WorkoutId = table.Column<int>(type: "int", nullable: true),
+                    WorkoutSessionId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Sets_WorkoutSessions_WorkoutSessionId",
+                        column: x => x.WorkoutSessionId,
+                        principalTable: "WorkoutSessions",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sets_WorkoutSessionId",
+                table: "Sets",
+                column: "WorkoutSessionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Exercises");
+
+            migrationBuilder.DropTable(
+                name: "Sets");
+
+            migrationBuilder.DropTable(
+                name: "workoutsTemplates");
+
+            migrationBuilder.DropTable(
+                name: "WorkoutSessions");
+        }
+    }
+}
